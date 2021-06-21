0be5220b7c26320e1cfcd623ea8259a5986d1177
Suhaili Faruq <suhaili.faruq01@gmail.com>
2021-06-21 10:28:38 +0700

    Downgrade kotlin version to Kotlin 1.4.32 Downgrade Gradle version to Gradle 6.11 Change Gradle code

--- FavoriteFeature/src/main/java/com/suhaili/favoritefeature/FavouriteActivity.kt
+++ FavoriteFeature/src/main/java/com/suhaili/favoritefeature/FavouriteActivity.kt
@@ -2,8 +2,6 @@ package com.suhaili.favoritefeature
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
